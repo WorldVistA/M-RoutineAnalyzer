2f735a7bfdb9f2d2f278c414564f68cae9d57c0b
Catalin Branea <cbranea@gmail.com>
2012-02-10 11:28:32 +0200

    Reports

--- src/ZZRGND15.m
+++ src/ZZRGND15.m
@@ -12,11 +12,19 @@ USES(GLB,PKG)  ; Prints globals used by a package and their owner
  . Q:$$GETPKG^ZZRGND19(RTN,GLB)'=PKG
  . S G=""
  . F  S G=$O(@GLB@(1,RTN,"G",G)) Q:G=""  D USESGLB(GLB,PKG,RTN,G) 
- W !,"Globals used through FileMan calls by ",$$GPKGNAME^ZZRGND19(PKG,GLB),!
+ W !!,"Globals used through FileMan calls by ",$$GPKGNAME^ZZRGND19(PKG,GLB),!
+ S RTN=""
+ F  S RTN=$O(@GLB@(7,PKG,RTN)) Q:RTN=""  D
+ . S TAG=""
+ . F  S TAG=$O(@GLB@(7,PKG,RTN,TAG)) Q:TAG=""  D
+ . . Q:'$D(@GLB@(7,PKG,RTN,TAG,"FMG"))
+ . . S G=""
+ . . F  S G=$O(@GLB@(7,PKG,RTN,TAG,"FMG",G)) Q:G=""  D USESGLB(GLB,PKG,RTN,G)
  Q
  ;
 USED(GLB,PKG) ; Prints packages that use globals owned by PKG
  N GINFO
+ S:$G(GLB)="" GLB="^ZZRG"
  S RTN="",CNT=0
  F  S RTN=$O(@GLB@(1,RTN)) Q:RTN=""  D
  . Q:$$GETPKG^ZZRGND19(RTN,GLB)=PKG
@@ -24,7 +32,7 @@ USED(GLB,PKG) ; Prints packages that use globals owned by PKG
  . F  S G=$O(@GLB@(1,RTN,"G",G)) Q:G=""  D
  . . S GINFO=$$GLBINFO(GLB,G)
  . . Q:$P(GINFO,"^",1)'=PKG
- . . W !,"Routine ",RTN,": ",G, " (",$$GPKGNAME^ZZRGND19($$GETPKG^ZZRGND19(RTN,GLB),GLB),")"
+ . . W !,"Routine ",RTN,": ",G," (",$$GPKGNAME^ZZRGND19($$GETPKG^ZZRGND19(RTN,GLB),GLB),")"
  Q
  ;
 USESGLB(GL,PKG,RTN,G)
